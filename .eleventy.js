5e90e717f5571139988c7f3982c0b650446ec01a
David Matthew <davidmatthew.ie@gmail.com>
2024-06-04 15:52:54 +0100

    Config updated with twig and yaml support and related shortcodes

--- .eleventy.js
+++ .eleventy.js
@@ -1,27 +1,57 @@
-const navigationPlugin = require('@11ty/eleventy-navigation');
 const vitePlugin = require('@11ty/eleventy-plugin-vite');
+const twig = require('twig');
+const yaml = require('js-yaml');
+const fs = require('fs');
 
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addPlugin(navigationPlugin);
-  eleventyConfig.addPlugin(vitePlugin);
+  eleventyConfig.addTemplateFormats('twig');
 
-  eleventyConfig.addPassthroughCopy({ 'inc/assets': '/' });
+  eleventyConfig.addExtension('twig', {
+    compile: async (inputContent, inputPath) => {
+      const template = twig.twig({
+        data: inputContent,
+        path: `./${inputPath}`,
+        namespaces: { components: './inc/components' },
+      });
+      return async (data) => {
+        return template.render(data);
+      };
+    },
+  });
 
-  eleventyConfig.addShortcode('getYear', function() {
+  twig.cache(false);
+
+  twig.extendFunction('getYear', () => {
     let date = new Date();
     return date.getFullYear();
   });
 
+  twig.extendFunction('getFile', (filePath) => {
+    return fs.readFileSync(filePath, 'utf8');
+  });
+
+  eleventyConfig.addDataExtension('yml', (contents) => {
+    return yaml.load(contents);
+  });
+
+  eleventyConfig.addPlugin(vitePlugin);
+
+  eleventyConfig.addPassthroughCopy({ 'inc/assets': '/' });
+
   return {
+    markdownTemplateEngine: 'twig',
+    htmlTemplateEngine: 'twig',
     templateFormats: [
+      'twig',
       'html',
-      'md'
+      'md'      
     ],
-    dir: { 
+    dir: {
       input: 'content',
       output: 'dist',
-      includes: '../inc'
+      includes: '../inc',
+      data: '../data'
     }
   };
   
