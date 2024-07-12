82032f1f7266d53bbe5bab44a31c93a4b75c6313
David Matthew <davidmatthew.ie@gmail.com>
2024-07-12 17:05:53 +0100

    Updates to colours and typography

--- inc/assets/js/pageNav.js
+++ inc/assets/js/pageNav.js
@@ -5,14 +5,28 @@ export default function pageNav() {
 
   // Check if the page contains the required navigation element.
   if (document.getElementById('page-nav')) {
- 
-    // Create and append the list element and heading.
-    const list = document.createElement('ul');
-    list.classList.add('ml-1');
-    const listHeading = document.createElement('h4');
-    listHeading.classList.add('font-bold', 'mb-1');
-    listHeading.innerText = 'On this page';
+
+    // Get the main page heading and page nav area.
+    const pageHeading = document.querySelector('h1');
     const pageNav = document.getElementById('page-nav');
+
+    // Create the required elements.
+    const list = document.createElement('ul');
+    const listHeading = document.createElement('h4');
+    const listHeadingLink = document.createElement('a');
+
+    // Create a link to the main page heading.
+    pageHeading.id = 'h1';
+    listHeadingLink.href = '#h1';
+
+    // Add styling and text.
+    pageNav.classList.add('text-granite-d3', 'dark:text-granite-l2');
+    list.classList.add('ml-1');
+    listHeading.classList.add('font-bold', 'mb-1', 'text-green', 'dark:text-modern-l1');
+    listHeadingLink.innerText = 'On this page';
+    
+    // Append the elements.
+    listHeading.appendChild(listHeadingLink);
     pageNav.appendChild(listHeading);
     pageNav.appendChild(list);
     
@@ -22,7 +36,7 @@ export default function pageNav() {
     // For each heading...
     subheadings.forEach((heading, index) => {
 
-      // Only run if data-page-nav isn't set to false.
+      // Run for all the h2 and h3 headings that haven't opted out.
       if (heading.dataset.pageNav != 'false') {
 
         // Create a list item and link.
@@ -35,11 +49,11 @@ export default function pageNav() {
 
         // Get the heading text and style it appropriately.
         link.innerText = heading.innerText;
-        listItem.classList.add('border-l', 'border-granite-l5', 'dark:border-granite');
+        listItem.classList.add('pb-1', 'border-l', 'border-granite-l3', 'dark:border-granite-d1');
         if (heading.tagName.toLowerCase() === 'h2') {
           listItem.classList.add('pl-3');
         } else {
-          listItem.classList.add('pl-5');
+          listItem.classList.add('pl-5', 'text-sm', 'text-granite-d1', 'dark:text-granite-l1');
         }
 
         // Append the elements.
