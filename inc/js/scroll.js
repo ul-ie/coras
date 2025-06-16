05704e0b0fc8cf5fd3ac3ac5d7c4d05c6ec549ca
David Matthew <davidmatthew.ie@gmail.com>
2025-06-16 14:47:52 +0100

    Added new scroll behaviour to header and logo

---
+++ inc/js/scroll.js
@@ -0,0 +1,30 @@
+/**
+ * Alter the top menu bar and logo on scroll.
+ */
+export default function scroll() {
+
+  let topBar = document.getElementById('top-bar');
+  let container = document.querySelector('#top-bar .container');
+  let logoText = document.querySelector('#main-logo svg #logo-text');
+
+  window.addEventListener('scroll', () => {
+
+    let thresholdReached = document.documentElement.scrollTop > 500 ? true : false;
+
+    if (thresholdReached) {
+      topBar.classList.remove('h-28');
+      topBar.classList.add('h-16');
+      container.classList.remove('py-4');
+      container.classList.add('py-2');
+      logoText.classList.remove('opacity-100');
+      logoText.classList.add('opacity-0');
+    } else {
+      topBar.classList.remove('h-16');
+      topBar.classList.add('h-28');
+      container.classList.remove('py-2');
+      container.classList.add('py-4');
+      logoText.classList.remove('opacity-0');
+      logoText.classList.add('opacity-100');
+    }
+  });
+}
