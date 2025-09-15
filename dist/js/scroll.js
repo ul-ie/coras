829eb1afede17bc9b2ad6fceef9517531c1dfc64
David Matthew <davidmatthew.ie@gmail.com>
2025-09-15 11:58:07 +0100

    Build updated

--- dist/js/scroll.js
+++ dist/js/scroll.js
@@ -3,28 +3,30 @@
  */
 export default function scroll() {
 
-  let topBar = document.getElementById('top-bar');
-  let container = document.querySelector('#top-bar .container');
-  let logoText = document.querySelector('#main-logo svg #logo-text');
+  const topBar = document.getElementById('top-bar');
+  const container = document.querySelector('#top-bar .container');
+  const logoText = document.querySelector('#main-logo svg #logo-text');
 
   window.addEventListener('scroll', () => {
 
-    let topBarThreshold = document.documentElement.scrollTop > 500 ? true : false;
+    if (topBar) {
+      let topBarThreshold = document.documentElement.scrollTop > 500 ? true : false;
 
-    if (topBarThreshold) {
-      topBar.classList.remove('h-28');
-      topBar.classList.add('h-16');
-      container.classList.remove('py-4');
-      container.classList.add('py-2');
-      logoText.classList.remove('opacity-100');
-      logoText.classList.add('opacity-0');
-    } else {
-      topBar.classList.remove('h-16');
-      topBar.classList.add('h-28');
-      container.classList.remove('py-2');
-      container.classList.add('py-4');
-      logoText.classList.remove('opacity-0');
-      logoText.classList.add('opacity-100');
+      if (topBarThreshold) {
+        topBar.classList.remove('h-28');
+        topBar.classList.add('h-16');
+        container.classList.remove('py-4');
+        container.classList.add('py-2');
+        logoText.classList.remove('opacity-100');
+        logoText.classList.add('opacity-0');
+      } else {
+        topBar.classList.remove('h-16');
+        topBar.classList.add('h-28');
+        container.classList.remove('py-2');
+        container.classList.add('py-4');
+        logoText.classList.remove('opacity-0');
+        logoText.classList.add('opacity-100');
+      }
     }
   });
 }
